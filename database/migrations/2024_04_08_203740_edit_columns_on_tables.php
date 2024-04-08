f935f2234d65bd241882aa9707cf47e4b6e6e8f8
HÔKWÈ Holding <hokweholding@gmail.com>
2024-04-08 22:41:34 +0200

    Add new migration

---
+++ database/migrations/2024_04_08_203740_edit_columns_on_tables.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+use Core\Utils\Traits\Database\Migrations\CanDeleteTrait;
+use Core\Utils\Traits\Database\Migrations\HasCompositeKey;
+use Core\Utils\Traits\Database\Migrations\HasForeignKey;
+use Core\Utils\Traits\Database\Migrations\HasTimestampsAndSoftDeletes;
+use Core\Utils\Traits\Database\Migrations\HasUuidPrimaryKey;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class `EditColumnsOnTables`
+ *
+ * A migration class for creating the "données" table with UUID primary key and timestamps.
+ *
+ * @package `\Database\Migrations\EditColumnsOnTables`
+ */
+class EditColumnsOnTables extends Migration
+{
+    use CanDeleteTrait, HasCompositeKey, HasForeignKey, HasTimestampsAndSoftDeletes, HasUuidPrimaryKey;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     *
+     * @throws \Core\Utils\Exceptions\DatabaseMigrationException If the migration fails.
+     */
+    public function up(): void
+    {
+        // Begin the database transaction
+        DB::beginTransaction();
+
+        try {
+
+            // Drop the foreign key constraint from the "devises" table
+            if (Schema::hasTable('devises')) {
+                if (Schema::hasColumn('devises', 'code')) {
+                    Schema::table('devises', function (Blueprint $table) {
+                        $table->dropColumn(['code']);
+                    });
+                }
+            }
+
+            // Commit the transaction
+            DB::commit();
+        } catch (\Throwable $exception) {
+            // Rollback the transaction in case of an exception
+            DB::rollBack();
+
+            // Handle the exception (e.g., logging, notification, etc.)
+            throw new \Core\Utils\Exceptions\DatabaseMigrationException(
+                message: 'Failed to migrate table: ' . $exception->getMessage(),
+                previous: $exception
+            );
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     *
+     * @throws \Core\Utils\Exceptions\DatabaseMigrationException If the migration fails.
+     */
+    public function down(): void
+    {
+
+    }
+}
