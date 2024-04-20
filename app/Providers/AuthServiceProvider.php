3d703d83e4867e46da7fb63a7bd936ac48948450
cocorine999 <118729336+cocorine999@users.noreply.github.com>
2024-04-21 01:12:17 +0200

    Corine (#38)
    * Correction au niveaux des employees, taux, salaire au postes (#32)
    
    * pull  of monday 08 april
    
    * correction of poste salary
    
    * Test
    
    ---------
    
    Co-authored-by: Paololibert <libertsinzo7@gmail.com>
    Co-authored-by: HÔKWÈ Holding <hokweholding@gmail.com>
    
    * Main (#34)
    
    * pull  of monday 08 april
    
    * correction of poste salary
    
    * Test
    
    * Correction au niveaux des employees, taux, salaire au postes (#32) (#33)
    
    * pull  of monday 08 april
    
    * correction of poste salary
    
    * Test
    
    ---------
    
    Co-authored-by: Paololibert <libertsinzo7@gmail.com>
    Co-authored-by: HÔKWÈ Holding <hokweholding@gmail.com>
    
    ---------
    
    Co-authored-by: Paololibert <libertsinzo7@gmail.com>
    Co-authored-by: HÔKWÈ Holding <hokweholding@gmail.com>
    
    * commit
    
    * Adding command
    
    * annuler Adding command
    
    * Documentation
    
    * Correction
    
    * Correction
    
    * Commit
    
    * Merge
    
    * Merge
    
    * Commit
    
    * Commit
    
    * API Documentation
    
    ---------
    
    Co-authored-by: Paololibert <libertsinzo7@gmail.com>
    Co-authored-by: HÔKWÈ Holding <hokweholding@gmail.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -54,7 +54,7 @@ class AuthServiceProvider extends ServiceProvider
                 return false;
             });  */
         } catch (Throwable $exception) {
-            throw new ApplicationException(previous: $exception);
+            report($exception);
         }
 
         Passport::tokensExpireIn(now()->addDays(15));
